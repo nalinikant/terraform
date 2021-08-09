12abe13466bd7a82ed765545fde5505382ca2262
nalinikant <nkm.dav@gmail.com>
2021-08-09 16:56:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,34 @@
+provider "aws"{
+    region = "us-east-2"
+    access_key = "AKIA6KZQ3UPJQBV6AKGC"
+    secret_key = "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"
+}
+resource "aws_instance" "adi_instance"{
+    ami = "ami-0233c2d874b811deb"
+    instance_type = "t2.micro"
+    security_groups = [aws_security_group.adi_sg.name]
+    depends_on = [aws_eip.adi_eip]
+}
 
+resource "aws_eip" "adi_eip"{
+    instance = aws_instance.adi_instance.id
+    depends_on = [aws_security_group.adi_sg]
+}
+resource "aws_security_group" "adi_sg"{
+    name = "Allow https"
+    ingress{
+        from_port = 443
+        to_port = 443 
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress{
+        from_port = 443
+        to_port = 443 
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+output "EIP"{
+value = aws_eip.adi_eip.public_ip
+}
